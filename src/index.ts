1eca885df3948f63b076567d59915d30f40b2257
MugishaU <muwiragiye123@gmail.com>
2023-08-16 15:04:50 +0100

    refactor split out price

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+export const getPrice = (product: string): number => {
+    switch (product){
+        case 'A':
+            return 50
+        case 'B':
+            return 30
+        case 'C':
+            return 20
+        case 'D':
+            return 15
+        default:
+            return 0
+    }
+}
+
 export const checkout = (products: string): number => {
-    return 0
+    const cost = products
+        .split('')
+        .map(product => getPrice(product))
+        .reduce((a,b) => a + b)
+
+    return cost
 }
--- test/checkout.test.ts
+++ test/checkout.test.ts
@@ -1,8 +1,49 @@
-import { checkout } from "../src"
+import { checkout, getPrice } from "../src"
 
-describe('checkout', () => {
-    it('has no items input', () => {
-        const result = checkout('')
-        expect(result).toBe(0)
+describe('index.ts', () => {
+    describe('getPrice', () => {
+        it('returns 0 when no products given', () => {
+            const result = getPrice('')
+            expect(result).toBe(0)
+        })
+        it('returns 50 for product A', () => {
+            const result = getPrice('A')
+            expect(result).toBe(50)
+        })
+        it('returns 30 for product B', () => {
+            const result = getPrice('B')
+            expect(result).toBe(30)
+        })
+        it('returns 20 for product C', () => {
+            const result = getPrice('C')
+            expect(result).toBe(20)
+        })
+        it('returns 15 for product D', () => {
+            const result = getPrice('D')
+            expect(result).toBe(15)
+        })
+    })
+
+    describe('checkout', () => {
+        it('returns 80 when product A and B given', () => {
+            const result = checkout('AB')
+            expect(result).toBe(80)
+        })
+    
+        it('returns 50 when product B and B given', () => {
+            const result = checkout('BC')
+            expect(result).toBe(50)
+        })
+    
+    
+        it('returns 65 when product A and D given', () => {
+            const result = checkout('DA')
+            expect(result).toBe(65)
+        })
+
+        it('returns 120 when given products: A, C, A', () => {
+            const result = checkout('ACA')
+            expect(result).toBe(120)
+        })
     })
 })
